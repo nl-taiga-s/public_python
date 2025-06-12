bb85f4846b8f365fabb9960f984acdae347a3511
b21k8002 <shimmura.taiga@marble-corp.co.jp>
2025-06-12 15:38:04 +0900

    created test of hello

---
+++ test_of_hello.py
@@ -0,0 +1,17 @@
+import hello
+
+
+# テスト関数: 指定されている関数の動作を確認するためのテストをする
+def test_func(monkeypatch):
+    # # 標準入力(ex.input関数)の回数分の戻り値をリストにする
+    # list_of_inputs = [str(1)] * 1
+    # # リストをイテレータに変換する
+    # iter_of_inputs = iter(list_of_inputs)
+    # # monkeypatchでinputをシミュレートする
+    # # input関数の引数が
+    # # ある場合 => lambda _
+    # # ない場合 => lambda
+    # # 標準入力の値を無名関数の戻り値のイテレータに置き換える
+    # monkeypatch.setattr("builtins.input", lambda _: next(iter_of_inputs))
+    # 指定された関数を呼び出す
+    hello.main()
