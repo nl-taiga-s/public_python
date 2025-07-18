225fc3852d7f77a4dd0840d55b5edc7041285aa8
b21k8002 <shimmura.taiga@marble-corp.co.jp>
2025-07-18 15:58:39 +0900

    fixed pt

--- source/pdf_tools/pt_class.py
+++ source/pdf_tools/pt_class.py
@@ -1,6 +1,9 @@
+import datetime
 from pathlib import Path
 
-from pypdf import PdfReader
+from pypdf import PdfReader, PdfWriter
+
+from source.common.common import DatetimeTools, PathTools
 
 
 class PdfTools:
@@ -9,17 +12,227 @@ class PdfTools:
     def __init__(self):
         """初期化します"""
         print(self.__class__.__doc__)
+        self.obj_of_dt2 = DatetimeTools()
+        self.obj_of_pt = PathTools()
+        self.reader = None
+        self.num_of_pages = None
+        self.writer = None
+        self.metadata_of_reader = None
+        self.metadata_of_writer = None
+        self.EXTENSION = ".pdf"
+        self.UTC_OF_JP = "+09'00'"
+        self.fields = [
+            ("title", "/Title"),  # タイトル
+            ("author", "/Author"),  # 作成者
+            ("subject", "/Subject"),  # サブタイトル
+            ("creator", "/Creator"),  # アプリケーション
+            ("producer", "/Producer"),  # PDF変換
+            ("keywords", "/Keywords"),  # キーワード
+            ("creation_date", "/CreationDate"),  # 作成日
+            ("modification_date", "/ModDate"),  # 更新日
+        ]
+        self.log = []
 
-    def read_metadata(self):
-        pdf_path = Path("~/pocket/sample_pptx.pdf").expanduser()
-        reader = PdfReader(pdf_path)
-        self.meta = reader.metadata
+    def encrypt(self, file_path: str, password: str) -> bool:
+        """暗号化します"""
+        try:
+            log_msg = None
+            self.reader = PdfReader(file_path)
+            self.writer = PdfWriter(clone_from=self.reader)
+            self.writer.encrypt(password, algorithm="AES-256")
+            with open(file_path, "wb") as f:
+                self.writer.write(f)
+        except Exception as e:
+            b = False
+            log_msg = f"暗号化に失敗しました。: {e}"
+        else:
+            b = True
+            log_msg = "暗号化に成功しました。"
+        finally:
+            print(log_msg)
+            time_stamp = self.obj_of_dt2.convert_dt_to_str(datetime.datetime.now())
+            self.log.append(f"{log_msg},{time_stamp}")
+            self.log.append(f"password: {password}")
+            return b
 
-    def print_metadata(self):
-        print(f"{self.meta.title}")
-        print(f"{self.meta.author}")
-        print(f"{self.meta.subject}")
-        print(f"{self.meta.creator}")
-        print(f"{self.meta.producer}")
-        print(f"{self.meta.creation_date}")
-        print(f"{self.meta.modification_date}")
+    def decrypt(self, file_path: str, password: str) -> bool:
+        """復号化します"""
+        try:
+            log_msg = None
+            self.reader = PdfReader(file_path)
+            self.reader.decrypt(password)
+            self.writer = PdfWriter(clone_from=self.reader)
+            with open(file_path, "wb") as f:
+                self.writer.write(f)
+        except Exception as e:
+            b = False
+            log_msg = f"復号化に失敗しました。: {e}"
+        else:
+            b = True
+            log_msg = "復号化に成功しました。"
+        finally:
+            print(log_msg)
+            time_stamp = self.obj_of_dt2.convert_dt_to_str(datetime.datetime.now())
+            self.log.append(f"{log_msg},{time_stamp}")
+            return b
+
+    def print_metadata(self) -> bool:
+        """メタデータを出力します"""
+        try:
+            log_msg = None
+            if self.meta_of_reader is None:
+                raise ValueError("出力するメタデータがありません。")
+            else:
+                for key, _ in self.fields:
+                    value = getattr(self.meta_of_reader, key, None)
+                    print(f"{key.capitalize().replace("_", " ")}: {value or None}")
+        except ValueError as e:
+            b = False
+            log_msg = str(e)
+        else:
+            b = True
+            log_msg = self.__class__.print_metadata.__doc__
+        finally:
+            print(log_msg)
+            time_stamp = self.obj_of_dt2.convert_dt_to_str(datetime.datetime.now())
+            self.log.append(f"{log_msg},{time_stamp}")
+            return b
+
+    def read_metadata(self, file_path: str) -> bool:
+        """メタデータを読み込みます"""
+        try:
+            log_msg = None
+            self.reader = PdfReader(file_path)
+            self.meta_of_reader = self.reader.metadata
+            if self.meta_of_reader is None:
+                raise ValueError("取得したメタデータがありません。")
+        except ValueError as e:
+            b = False
+            log_msg = str(e)
+        else:
+            b = True
+            log_msg = self.__class__.read_metadata.__doc__
+        finally:
+            print(log_msg)
+            time_stamp = self.obj_of_dt2.convert_dt_to_str(datetime.datetime.now())
+            self.log.append(f"{log_msg},{time_stamp}")
+            return b
+
+    def write_metadata(self, file_path: str) -> bool:
+        """メタデータを書き込みます"""
+        try:
+            log_msg = None
+            self.writer = PdfWriter()
+            for page in self.reader.pages:
+                self.writer.add_page(page)
+            self.metadata_of_writer = {}
+            for key_of_r, key_of_w in self.fields:
+                match key_of_r:
+                    case "creation_date":
+                        pass
+                    case "modification_date":
+                        time = self.obj_of_dt2.convert_for_metadata_in_pdf(self.UTC_OF_JP)
+                        self.metadata_of_writer[f"{key_of_w}"] = time
+                    case _:
+                        value = input(f"{key_of_r.capitalize().replace("_", " ")}: ")
+                        self.metadata_of_writer[f"{key_of_w}"] = value
+            self.writer.add_metadata(self.metadata_of_writer)
+            with open(file_path, "wb") as f:
+                self.writer.write(f)
+        except Exception as e:
+            b = False
+            log_msg = str(e)
+        else:
+            b = True
+            log_msg = self.__class__.write_metadata.__doc__
+        finally:
+            print(log_msg)
+            time_stamp = self.obj_of_dt2.convert_dt_to_str(datetime.datetime.now())
+            self.log.append(f"{log_msg},{time_stamp}")
+            return b
+
+    def merge(self, pdfs: list) -> bool:
+        """マージします"""
+        try:
+            log_msg = None
+            first_file_of_pdf_as_path_type = Path(pdfs[0])
+            folder_of_pdf_as_path_type = self.obj_of_pt.get_dir_path(first_file_of_pdf_as_path_type)
+            dt = self.obj_of_dt2.convert_for_file_name()
+            file_name_of_pdf_as_str_type = f"merged_file_{dt}.pdf"
+            file_of_pdf_as_path_type = folder_of_pdf_as_path_type / file_name_of_pdf_as_str_type
+            file_path_of_pdf_as_str_type = str(file_of_pdf_as_path_type)
+            self.writer = PdfWriter()
+            for pdf in pdfs:
+                self.writer.append(pdf)
+            with open(file_path_of_pdf_as_str_type, "wb") as f:
+                self.writer.write(f)
+        except Exception as e:
+            b = False
+            log_msg = f"マージが失敗しました。: {e}"
+        else:
+            b = True
+            log_msg = "マージが成功しました。"
+        finally:
+            print(log_msg)
+            time_stamp = self.obj_of_dt2.convert_dt_to_str(datetime.datetime.now())
+            self.log.append(f"{log_msg},{time_stamp}")
+            return b
+
+    def extract_pages(self, file_path: str, begin_page: int, end_page: int) -> bool:
+        """ページを抽出します"""
+        try:
+            log_msg = None
+            self.reader = PdfReader(file_path)
+            self.writer = PdfWriter()
+            for i in range(begin_page - 1, end_page):
+                self.writer.add_page(self.reader.pages[i])
+            file_of_exe_as_path_type = Path(file_path)
+            folder_of_pdf_as_path_type = self.obj_of_pt.get_dir_path(file_of_exe_as_path_type)
+            dt = self.obj_of_dt2.convert_for_file_name()
+            file_name_of_pdf_as_str_type = f"extracted_file_{dt}.pdf"
+            file_of_pdf_as_path_type = folder_of_pdf_as_path_type / file_name_of_pdf_as_str_type
+            file_path_of_pdf_as_str_type = str(file_of_pdf_as_path_type)
+            with open(file_path_of_pdf_as_str_type, "wb") as f:
+                self.writer.write(f)
+        except Exception as e:
+            b = False
+            log_msg = f"ページの抽出に失敗しました。: {e}"
+        else:
+            b = True
+            log_msg = "ページの抽出に成功しました。"
+        finally:
+            print(log_msg)
+            time_stamp = self.obj_of_dt2.convert_dt_to_str(datetime.datetime.now())
+            self.log.append(f"{log_msg},{time_stamp}")
+            return b
+
+    def extract_text(self, file_path: str, begin_page: int, end_page: int) -> bool:
+        """テキストを抽出します"""
+        try:
+            log_msg = None
+            self.reader = PdfReader(file_path)
+            for i in range(begin_page - 1, end_page):
+                print(f"{i + 1}ページ: ")
+                print(self.reader.pages[i].extract_text())
+        except Exception as e:
+            b = False
+            log_msg = f"テキストの抽出に失敗しました。: {e}"
+        else:
+            b = True
+            log_msg = "テキストの抽出に成功しました。"
+        finally:
+            print(log_msg)
+            time_stamp = self.obj_of_dt2.convert_dt_to_str(datetime.datetime.now())
+            self.log.append(f"{log_msg},{time_stamp}")
+            return b
+
+    def write_log(self, file_of_log_as_path_type: Path):
+        """処理結果をログに書き出す"""
+        file_of_log_as_str_type = str(file_of_log_as_path_type)
+        try:
+            with open(file_of_log_as_str_type, "w", encoding="utf-8", newline="") as f:
+                f.write("\n".join(self.log))
+        except Exception as e:
+            print(f"ログファイルの出力に失敗しました。: \n{e}")
+        else:
+            print(f"ログファイルの出力に成功しました。: \n{file_of_log_as_str_type}")
--- source/pdf_tools/pt_with_cui.py
+++ source/pdf_tools/pt_with_cui.py
@@ -1,10 +1,256 @@
+import os
+import re
+from pathlib import Path
+
 from pt_class import PdfTools
+from pypdf import PdfReader
+
+from source.common.common import PathTools
 
 
-def main():
-    obj = PdfTools()
-    obj.read_metadata()
+class PT:
+    def __init__(self):
+        """初期化します"""
+        self.d_of_bool = {
+            "yes": ["はい", "1", "Yes", "yes", "Y", "y"],
+            "no": ["いいえ", "0", "No", "no", "N", "n"],
+        }
+        self.obj_of_pt = PathTools()
+        self.obj_of_cls = PdfTools()
+        self.result = False
+
+    def input_target_of_pdf(self) -> str:
+        """対象のPDFファイルのパスを入力します。"""
+        try:
+            TILDE = "~"
+            file_path_of_pdf_as_str_type = input("対象のPDFファイルのパスを入力してください。: ")
+            file_of_pdf_as_path_type = Path(file_path_of_pdf_as_str_type)
+            if TILDE in file_path_of_pdf_as_str_type:
+                file_of_pdf_as_path_type = self.obj_of_pt.get_expanded_in_home_dir(file_of_pdf_as_path_type)
+                file_path_of_pdf_as_str_type = str(file_of_pdf_as_path_type)
+            if not file_of_pdf_as_path_type.exists() or self.obj_of_pt.get_extension(file_of_pdf_as_path_type) != self.obj_of_cls.EXTENSION:
+                raise FileNotFoundError(f"ファイルが不正です。: {file_path_of_pdf_as_str_type}")
+            self.obj_of_cls.reader = PdfReader(file_path_of_pdf_as_str_type)
+            self.obj_of_cls.num_of_pages = len(self.obj_of_cls.reader.pages)
+            return file_path_of_pdf_as_str_type
+        except FileNotFoundError as e:
+            print(e)
+        except KeyboardInterrupt:
+            os._exit(0)
+
+    def input_bool_of_encrypt(self) -> bool:
+        """暗号化するかどうかを入力します"""
+        try:
+            if self.obj_of_cls.reader.is_encrypted:
+                print("既に暗号化されています。")
+            else:
+                while True:
+                    str_of_bool = input("暗号化するかどうかを入力してください。: ")
+                    match str_of_bool:
+                        case var if var in self.d_of_bool["yes"]:
+                            return True
+                        case var if var in self.d_of_bool["no"]:
+                            return False
+                        case _:
+                            raise ValueError(f"無効な入力です。: {str_of_bool}")
+        except ValueError as e:
+            print(e)
+        except KeyboardInterrupt:
+            os._exit(0)
+
+    def input_password_of_encrypt(self) -> str:
+        """暗号化のパスワードを入力します"""
+        try:
+            while True:
+                password = input("暗号化のパスワードを入力してください。: ")
+                if re.fullmatch(r"[A-Za-z0-9_-]+", password):
+                    return password
+                else:
+                    print("以下の文字で入力してください。\n* 半角英数字\n* アンダーバー\n* ハイフン")
+        except KeyboardInterrupt:
+            os._exit(0)
+
+    def input_bool_of_decrypt(self) -> bool:
+        """復号化するかどうかを入力します"""
+        try:
+            if not self.obj_of_cls.reader.is_encrypted:
+                print("既に復号化されています。")
+            else:
+                while True:
+                    str_of_bool = input("復号化するかどうかを入力してください。: ")
+                    match str_of_bool:
+                        case var if var in self.d_of_bool["yes"]:
+                            return True
+                        case var if var in self.d_of_bool["no"]:
+                            return False
+                        case _:
+                            raise ValueError(f"無効な入力です。: {str_of_bool}")
+        except ValueError as e:
+            print(e)
+        except KeyboardInterrupt:
+            os._exit(0)
+
+    def input_password_of_decrypt(self) -> str:
+        """復号化のパスワードを入力します"""
+        try:
+            while True:
+                password = input("復号化のパスワードを入力してください。: ")
+                if re.fullmatch(r"[A-Za-z0-9_-]+", password):
+                    return password
+                else:
+                    print("以下の文字で入力してください。\n* 半角英数字\n* アンダーバー\n* ハイフン")
+        except KeyboardInterrupt:
+            os._exit(0)
+
+    def input_bool_of_merge(self) -> bool:
+        """マージするかどうかを入力します"""
+        try:
+            while True:
+                str_of_bool = input("マージするかどうかを入力してください。: ")
+                match str_of_bool:
+                    case var if var in self.d_of_bool["yes"]:
+                        return True
+                    case var if var in self.d_of_bool["no"]:
+                        return False
+                    case _:
+                        raise ValueError(f"無効な入力です。: {str_of_bool}")
+        except ValueError as e:
+            print(e)
+        except KeyboardInterrupt:
+            os._exit(0)
+
+    def input_list_of_merge(self) -> list:
+        """マージ元の全てのファイルを入力します"""
+        try:
+            pdfs = []
+            while True:
+                file_as_str_type = input("マージ元のファイルのパスを順番に入力してください。: ")
+                file_as_path_type = Path(file_as_str_type)
+                if file_as_path_type.exists() and self.obj_of_pt.get_extension(file_as_path_type) == self.obj_of_cls.EXTENSION:
+                    pdfs.append(file_as_str_type)
+                else:
+                    print("有効なマージ元のファイルのパスを入力してください。")
+                str_of_bool = input("対象のファイルは、まだありますか？: ")
+                match str_of_bool:
+                    case var if var in self.d_of_bool["yes"]:
+                        pass
+                    case var if var in self.d_of_bool["no"]:
+                        break
+                    case _:
+                        raise ValueError(f"無効な入力です。: {str_of_bool}")
+            return pdfs
+        except ValueError as e:
+            print(e)
+        except KeyboardInterrupt:
+            os._exit(0)
+
+    def input_bool_of_extracting_pages(self) -> bool:
+        """ページを抽出するかどうかを入力します"""
+        try:
+            while True:
+                str_of_bool = input("ページを抽出するかどうかを入力してください。: ")
+                match str_of_bool:
+                    case var if var in self.d_of_bool["yes"]:
+                        return True
+                    case var if var in self.d_of_bool["no"]:
+                        return False
+                    case _:
+                        raise ValueError(f"無効な入力です。: {str_of_bool}")
+        except ValueError as e:
+            print(e)
+        except KeyboardInterrupt:
+            os._exit(0)
+
+    def input_extracting_pages(self) -> list:
+        """抽出するページを入力します"""
+        try:
+            while True:
+                print(self.__class__.input_extracting_pages.__doc__)
+                begin_page = input("始めのページを入力してください。: ")
+                end_page = input("終わりのページを入力してください。: ")
+                if not begin_page.isdigit() and end_page.isdigit():
+                    print("数字を入力してください。")
+                    continue
+                begin_page = int(begin_page)
+                end_page = int(end_page)
+                if begin_page < 1 or end_page > self.obj_of_cls.num_of_pages or begin_page > end_page:
+                    raise ValueError("指定のページ範囲が不正です。")
+                else:
+                    break
+            return begin_page, end_page
+        except ValueError as e:
+            print(e)
+        except KeyboardInterrupt:
+            os._exit(0)
+
+    def input_bool_of_extracting_text(self) -> bool:
+        """テキストを抽出するかどうかを入力します"""
+        try:
+            while True:
+                str_of_bool = input("テキストを抽出するかどうかを入力してください。: ")
+                match str_of_bool:
+                    case var if var in self.d_of_bool["yes"]:
+                        return True
+                    case var if var in self.d_of_bool["no"]:
+                        return False
+                    case _:
+                        raise ValueError(f"無効な入力です。: {str_of_bool}")
+        except ValueError as e:
+            print(e)
+        except KeyboardInterrupt:
+            os._exit(0)
+
+    def input_pages_of_extracting_text(self) -> list:
+        """テキストを抽出するページを入力します"""
+        try:
+            while True:
+                print(self.__class__.input_pages_of_extracting_text.__doc__)
+                begin_page = input("始めのページを入力してください。: ")
+                end_page = input("終わりのページを入力してください。: ")
+                if not begin_page.isdigit() and end_page.isdigit():
+                    print("数字を入力してください。")
+                    continue
+                begin_page = int(begin_page)
+                end_page = int(end_page)
+                if begin_page < 1 or end_page > self.obj_of_cls.num_of_pages or begin_page > end_page:
+                    raise ValueError("指定のページ範囲が不正です。")
+                else:
+                    break
+            return begin_page, end_page
+        except ValueError as e:
+            print(e)
+        except KeyboardInterrupt:
+            os._exit(0)
+
+    def main(self):
+        """主要関数"""
+        file_path_of_pdf_as_str_type = self.input_target_of_pdf()
+        if self.input_bool_of_encrypt():
+            password = self.input_password_of_encrypt()
+            self.result = self.obj_of_cls.encrypt(file_path_of_pdf_as_str_type, password)
+        elif self.input_bool_of_decrypt():
+            password = self.input_password_of_decrypt()
+            self.result = self.obj_of_cls.decrypt(file_path_of_pdf_as_str_type, password)
+        if not self.result:
+            self.obj_of_cls.read_metadata(file_path_of_pdf_as_str_type)
+            self.obj_of_cls.print_metadata()
+            self.obj_of_cls.write_metadata(file_path_of_pdf_as_str_type)
+            self.obj_of_cls.read_metadata(file_path_of_pdf_as_str_type)
+            self.obj_of_cls.print_metadata()
+            if self.input_bool_of_merge():
+                pdfs = self.input_list_of_merge()
+                self.obj_of_cls.merge(pdfs)
+            if self.input_bool_of_extracting_pages():
+                begin_page, end_page = self.input_extracting_pages()
+                self.obj_of_cls.extract_pages(file_path_of_pdf_as_str_type, begin_page, end_page)
+            if self.input_bool_of_extracting_text():
+                begin_page, end_of_page = self.input_pages_of_extracting_text()
+                self.obj_of_cls.extract_text(file_path_of_pdf_as_str_type, begin_page, end_page)
+        file_of_exe_as_path_type = Path(__file__)
+        file_of_log_as_path_type = self.obj_of_pt.get_file_path_of_log(file_of_exe_as_path_type)
+        self.obj_of_cls.write_log(file_of_log_as_path_type)
 
 
 if __name__ == "__main__":
-    main()
+    obj_with_cui = PT()
+    obj_with_cui.main()
