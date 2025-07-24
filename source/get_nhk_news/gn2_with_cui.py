00fb79e244a4efcfee9ad05547a679e74554dcb5
b21k8002 <shimmura.taiga@marble-corp.co.jp>
2025-07-24 18:56:01 +0900

    fixed gn2

--- source/get_nhk_news/gn2_with_cui.py
+++ source/get_nhk_news/gn2_with_cui.py
@@ -1,4 +1,4 @@
-import os
+import sys
 from pathlib import Path
 
 from source.common.common import PathTools
@@ -16,20 +16,27 @@ class GN2:
             while True:
                 print("ジャンルを選択してください。")
                 genres = list(d.keys())
-                for i, genre in enumerate(genres):
+                for i, genre in enumerate(genres, 1):
                     print(f"{i}: {genre}")
-                choice = int(input("番号を入力してください。: "))
-                return choice, genres[choice]
-        except (ValueError, IndexError):
-            print("無効な入力です。")
+                choice = input("番号を入力してください。: ")
+                if choice == "":
+                    return [None, None]
+                elif choice.isdecimal():
+                    choice = int(choice)
+                    if 1 <= choice and choice <= len(genres):
+                        return [choice, genres[choice - 1]]
+        except Exception as e:
+            print(e)
         except KeyboardInterrupt:
-            os._exit(0)
+            sys.exit(0)
 
-    def main(self):
+    def main(self) -> bool:
         """主要関数"""
         self.obj_of_cls = GetNHKNews()
         # ジャンルを選択します
         num_of_genre, key_of_genre = self.select_genre(self.obj_of_cls.rss_feeds)
+        if num_of_genre is None and key_of_genre is None:
+            return False
         self.obj_of_cls.parse_rss(num_of_genre, key_of_genre)
         self.obj_of_cls.get_standard_time_and_today(self.obj_of_cls.TIMEZONE_OF_JAPAN)
         self.obj_of_cls.extract_news_of_today_from_standard_time()
@@ -37,6 +44,7 @@ class GN2:
         file_of_exe_as_path_type = Path(__file__)
         file_of_log_as_path_type = self.obj_of_pt.get_file_path_of_log(file_of_exe_as_path_type)
         self.obj_of_cls.write_log(file_of_log_as_path_type)
+        return True
 
 
 if __name__ == "__main__":
