bc3641e0837ae59f7198b1634df37f8e0d19e330
b21k8002 <shimmura.taiga@marble-corp.co.jp>
2025-07-24 18:55:25 +0900

    fixed gfl

--- source/get_file_list/gfl_with_cui.py
+++ source/get_file_list/gfl_with_cui.py
@@ -1,4 +1,4 @@
-import os
+import sys
 from pathlib import Path
 
 from source.common.common import DatetimeTools, PathTools
@@ -21,8 +21,7 @@ class GFL:
             while True:
                 folder_path = input("ファイルを検索したいフォルダを入力してください。: ")
                 if folder_path == "":
-                    print("処理を中止します。")
-                    return
+                    return None
                 folder_as_path_type = Path(folder_path)
                 if folder_as_path_type.exists():
                     # 存在する場合
@@ -30,7 +29,7 @@ class GFL:
                         # フォルダの場合
                         return folder_path
         except KeyboardInterrupt:
-            os._exit(0)
+            sys.exit(0)
 
     def input_bool_of_recursive(self) -> bool:
         """フォルダを再帰的に検索するかどうかを入力します"""
@@ -47,7 +46,7 @@ class GFL:
         except ValueError as e:
             print(e)
         except KeyboardInterrupt:
-            os._exit(0)
+            sys.exit(0)
 
     def input_pattern(self) -> str:
         """検索パターンを入力します"""
@@ -55,11 +54,13 @@ class GFL:
             pattern = input("ファイルの検索パターンを入力してください。: ")
             return pattern
         except KeyboardInterrupt:
-            os._exit(0)
+            sys.exit(0)
 
-    def main(self):
+    def main(self) -> bool:
         """主要関数"""
         folder_path = self.input_folder_path()
+        if folder_path is None:
+            return False
         bool_of_r = self.input_bool_of_recursive()
         obj_of_cls = GetFileList(folder_path, bool_of_r)
         pattern = self.input_pattern()
@@ -68,6 +69,7 @@ class GFL:
         file_of_exe_as_path_type = Path(__file__)
         file_of_log_as_path_type = self.obj_of_pt.get_file_path_of_log(file_of_exe_as_path_type)
         obj_of_cls.write_log(file_of_log_as_path_type, obj_of_cls.list_file_after)
+        return True
 
 
 if __name__ == "__main__":
--- tests/get_file_list/test_of_gfl_with_cui.py
+++ tests/get_file_list/test_of_gfl_with_cui.py
@@ -4,7 +4,7 @@ from source.get_file_list import gfl_with_cui
 # テスト関数: 指定されている関数の動作を確認するためのテストをする
 def test_func(monkeypatch):
     # 標準入力(ex.input関数)の回数分の戻り値をリストにする
-    list_of_inputs = [""] * 1
+    list_of_inputs = [""]
     # リストをイテレータに変換する
     iter_of_inputs = iter(list_of_inputs)
     # monkeypatchでinputをシミュレートする
