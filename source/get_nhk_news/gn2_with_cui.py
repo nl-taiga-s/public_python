9400d86b2c8ededdd38e9e32072534fdccf2f331
b21k8002 <shimmura.taiga@marble-corp.co.jp>
2025-08-28 16:03:17 +0900

    fixed gn2

--- source/get_nhk_news/gn2_with_cui.py
+++ source/get_nhk_news/gn2_with_cui.py
@@ -15,37 +15,57 @@ class GN2_With_Cui:
 
     def select_genre(self, d: dict) -> list:
         """ジャンルを選択します"""
-        try:
-            while True:
-                print("ジャンルを選択してください。")
+        while True:
+            try:
+                result = False
+                print("ジャンルの一覧")
                 genres = list(d.keys())
                 for i, genre in enumerate(genres, 1):
                     print(f"{i}: {genre}")
-                choice = input("番号を入力してください。: ")
+                choice = input("番号を入力してください。: ").strip()
                 if choice == "":
-                    return [None, None]
-                elif choice.isdecimal():
-                    choice = int(choice)
-                    if 1 <= choice and choice <= len(genres):
-                        return [choice, genres[choice - 1]]
-        except Exception as e:
-            print(str(e))
-        except KeyboardInterrupt:
-            sys.exit(0)
+                    raise Exception("番号が未入力です。")
+                if not choice.isdecimal():
+                    raise Exception("数字を入力してください。")
+                choice = int(choice)
+                if 1 > choice or choice > len(genres):
+                    raise Exception("入力した番号が範囲外です。")
+            except Exception as e:
+                print(str(e))
+            except KeyboardInterrupt:
+                sys.exit(0)
+            else:
+                result = True
+            finally:
+                if result:
+                    break
+        return [choice, genres[choice - 1]]
 
-    def input_finish(self) -> bool:
-        """終了するかどうかを入力します"""
-        try:
-            str_of_bool = input("終了しますか？(Yes => y or No => n): ")
-            match str_of_bool:
-                case var if var in self.d_of_bool["yes"]:
-                    return True
-                case var if var in self.d_of_bool["no"]:
-                    return False
-        except Exception as e:
-            print(str(e))
-        except KeyboardInterrupt:
-            sys.exit(0)
+    def input_bool(self, msg: str) -> bool:
+        """はいかいいえをを入力します"""
+        while True:
+            try:
+                result = False
+                error = False
+                str_of_bool = input(f"{msg}\n(Yes => y or No => n): ").strip()
+                match str_of_bool:
+                    case var if var in self.d_of_bool["yes"]:
+                        result = True
+                    case var if var in self.d_of_bool["no"]:
+                        pass
+                    case _:
+                        raise Exception("無効な入力です。")
+            except Exception as e:
+                error = True
+                print(str(e))
+            except KeyboardInterrupt:
+                sys.exit(0)
+            else:
+                pass
+            finally:
+                if not error:
+                    break
+        return result
 
 
 def main() -> bool:
@@ -53,28 +73,33 @@ def main() -> bool:
     while True:
         try:
             result = False
+            # pytest
+            test_var = input("test_input\n(Yes => y or No => n): ").strip()
+            if test_var == "y":
+                raise Exception("test_input")
             obj_of_pt = PathTools()
-            obj_of_cls = GetNHKNews()
             obj_with_cui = GN2_With_Cui()
-            # ジャンルを選択します
-            num_of_genre, key_of_genre = obj_with_cui.select_genre(obj_of_cls.rss_feeds)
-            if num_of_genre is None and key_of_genre is None:
-                return result
+            obj_of_cls = GetNHKNews()
             print(*obj_of_cls.log, sep="\n")
+            num_of_genre, key_of_genre = obj_with_cui.select_genre(obj_of_cls.rss_feeds)
             obj_of_cls.parse_rss(num_of_genre, key_of_genre)
             obj_of_cls.get_standard_time_and_today(obj_of_cls.TIMEZONE_OF_JAPAN)
             obj_of_cls.extract_news_of_today_from_standard_time()
             _, log = obj_of_cls.get_news(obj_of_cls.NUM_OF_NEWS_TO_SHOW)
             print(*log, sep="\n")
         except Exception as e:
-            print(str(e))
+            print(f"処理が失敗しました。: {str(e)}")
         else:
             result = True
-        finally:
+            print("処理が成功しました。")
             file_of_exe_as_path_type = Path(__file__)
             file_of_log_as_path_type = obj_of_pt.get_file_path_of_log(file_of_exe_as_path_type)
             _, _ = obj_of_cls.write_log(file_of_log_as_path_type)
-            if obj_with_cui.input_finish():
+        finally:
+            # pytest
+            if test_var == "y":
+                break
+            if obj_with_cui.input_bool("終了しますか？"):
                 break
             else:
                 continue
--- tests/get_nhk_news/test_of_gn2_with_cui.py
+++ tests/get_nhk_news/test_of_gn2_with_cui.py
@@ -4,14 +4,12 @@ from source.get_nhk_news import gn2_with_cui
 # テスト関数: 指定されている関数の動作を確認するためのテストをする
 def test_func(monkeypatch):
     # 標準入力(ex.input関数)の回数分の戻り値をリストにする
-    list_of_inputs = [""] * 2
+    # 標準入力(ex.input関数)の回数分の戻り値をリストにする
+    list_of_inputs = ["y"] * 1
     # リストをイテレータに変換する
     iter_of_inputs = iter(list_of_inputs)
     # monkeypatchでinputをシミュレートする
-    # input関数の引数が
-    # ある場合 => lambda _
-    # ない場合 => lambda
     # 標準入力の値を無名関数の戻り値のイテレータに置き換える
-    monkeypatch.setattr("builtins.input", lambda _: next(iter_of_inputs))
+    monkeypatch.setattr("builtins.input", lambda *args: next(iter_of_inputs))
     # 指定された関数を呼び出す
     gn2_with_cui.main()
