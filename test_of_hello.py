3d7cf20773b1a914a30a767e4e17e2143afd1dbe
b21k8002 <shimmura.taiga@marble-corp.co.jp>
2025-10-02 10:51:55 +0900

    fixed test of hello

--- test_of_hello.py
+++ test_of_hello.py
@@ -4,9 +4,9 @@ import hello
 # テスト関数: 指定されている関数の動作を確認するためのテストをする
 def test_func(monkeypatch):
     # # 標準入力(ex.input関数)の回数分の戻り値をリストにする
-    # list_of_inputs = [str(1)] * 1
+    # lst_of_inputs = [str(1)] * 1
     # # リストをイテレータに変換する
-    # iter_of_inputs = iter(list_of_inputs)
+    # iter_of_inputs = iter(lst_of_inputs)
     # # monkeypatchでinputをシミュレートする
     # # 標準入力の値を無名関数の戻り値のイテレータに置き換える
     # monkeypatch.setattr("builtins.input", lambda *args: next(iter_of_inputs))
