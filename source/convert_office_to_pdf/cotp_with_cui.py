efef065842c7c91994e2057b90b595d7b885566f
b21k8002 <shimmura.taiga@marble-corp.co.jp>
2025-12-05 13:21:35 +0900

    fixed cotp

--- source/convert_office_to_pdf/cotp_with_cui.py
+++ source/convert_office_to_pdf/cotp_with_cui.py
@@ -12,7 +12,7 @@ class COTP_With_Cui:
             "no": ["いいえ", "0", "No", "no", "N", "n"],
         }
 
-    def _input_folder_path(self) -> list:
+    def _input_folder_path(self) -> tuple:
         """フォルダのパスを入力します"""
         while True:
             try:
@@ -30,13 +30,13 @@ class COTP_With_Cui:
                     raise Exception("フォルダではありません。")
             except KeyboardInterrupt:
                 raise
-            except Exception:
-                raise
+            except Exception as e:
+                print(f"error: \n{str(e)}")
             else:
                 break
             finally:
                 pass
-        return [folder_from_s, folder_to_s]
+        return (folder_from_s, folder_to_s)
 
     def _input_bool(self, msg: str) -> bool:
         """はいかいいえをを入力します"""
@@ -53,8 +53,8 @@ class COTP_With_Cui:
                         raise Exception("無効な入力です。")
             except KeyboardInterrupt:
                 raise
-            except Exception:
-                raise
+            except Exception as e:
+                print(f"error: \n{str(e)}")
             else:
                 break
             finally:
