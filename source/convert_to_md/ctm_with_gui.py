64d434fd829cd7abdcdde7815f0e81ea3720d951
b21k8002 <shimmura.taiga@marble-corp.co.jp>
2025-12-18 11:26:40 +0900

    fixed ctm

--- source/convert_to_md/ctm_with_gui.py
+++ source/convert_to_md/ctm_with_gui.py
@@ -208,7 +208,7 @@ class MainApp_Of_CTM(QMainWindow):
             pass
         return result
 
-    def open_explorer(self, folder_path: str = "") -> bool:
+    def open_explorer(self, folder_path: str) -> bool:
         """エクスプローラーを開きます"""
         result: bool = False
         EXPLORER_OF_WSL: str = "/mnt/c/Windows/explorer.exe"
