b2604d823083bf17b832c8de349df62fa0b93627
b21k8002 <shimmura.taiga@marble-corp.co.jp>
2025-12-23 14:51:39 +0900

    renamed and fixed main

---
+++ source/common/main.py
@@ -0,0 +1,130 @@
+import asyncio
+import inspect
+import sys
+
+
+def main() -> bool:
+    """Please select one from Useful Tools"""
+    result: bool = False
+    try:
+        if len(sys.argv) == 1:
+            run_gui_launcher()
+        elif len(sys.argv) == 3:
+            mode: str = sys.argv[1]
+            tool: str = sys.argv[2]
+            match mode:
+                case "cui":
+                    run_with_cui(tool)
+                case "gui":
+                    run_with_gui(tool)
+                case _:
+                    raise
+        else:
+            print_usage()
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except Exception as e:
+        print(f"***{main.__doc__} => 失敗しました。***: \n{str(e)}")
+    else:
+        result = True
+    finally:
+        pass
+    return result
+
+
+def run_with_cui(tool: str) -> bool:
+    """CUIで実行します"""
+    result: bool = False
+    try:
+        match tool:
+            case "cltp":
+                from source.convert_libre_to_pdf.cltp_with_cui import main
+            case "cotp":
+                from source.convert_office_to_pdf.cotp_with_cui import main
+            case "ctm":
+                from source.convert_to_md.ctm_with_cui import main
+            case "gfl":
+                from source.get_file_list.gfl_with_cui import main
+            case "g2s":
+                from source.get_government_statistics.g2s_with_cui import main
+            case "pt":
+                from source.pdf_tools.pt_with_cui import main
+            case _:
+                raise
+        if inspect.iscoroutinefunction(main):
+            asyncio.run(main())
+        else:
+            main()
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except Exception:
+        raise
+    else:
+        result = True
+    finally:
+        pass
+    return result
+
+
+def run_with_gui(tool: str) -> bool:
+    """GUIで実行します"""
+    result: bool = False
+    try:
+        match tool:
+            case "cltp":
+                from source.convert_libre_to_pdf.cltp_with_gui import main
+            case "cotp":
+                from source.convert_office_to_pdf.cotp_with_gui import main
+            case "ctm":
+                from source.convert_to_md.ctm_with_gui import main
+            case "gfl":
+                from source.get_file_list.gfl_with_gui import main
+            case "g2s":
+                from source.get_government_statistics.g2s_with_gui import main
+            case "pt":
+                from source.pdf_tools.pt_with_gui import main
+            case _:
+                raise
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except Exception:
+        raise
+    else:
+        result = True
+    finally:
+        pass
+    return result
+
+
+def run_gui_launcher() -> bool:
+    """GUIランチャーを実行します"""
+    result: bool = False
+    try:
+        from gui_launcher import main
+
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except Exception:
+        raise
+    else:
+        result = True
+    finally:
+        pass
+    return result
+
+
+def print_usage():
+    """使用方法を表示します"""
+    print(
+        "usage: ",
+        "main.exe => GUI Launcher",
+        "main.exe cui <tool_name> => CUI",
+        "main.exe gui <tool_name> => GUI",
+        sep="\n",
+    )
+
+
+if __name__ == "__main__":
+    main()
